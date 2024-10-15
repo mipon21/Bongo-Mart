619eb9004ea68f81ff3471c24050d29788cfcb2d
mipon21 <mipon5500u@gmail.com>
2024-10-15 10:11:57 -0700

    Navigation menu updated

--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
 import 'package:bongo_mart/features/shop/cart/cart_screen.dart';
 import 'package:bongo_mart/features/personalization/screens/settings/settings.dart';
 import 'package:bongo_mart/features/shop/screens/favourite/favourite_screen.dart';
@@ -16,33 +14,62 @@ class NavigationMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final controller  = Get.put(NavigationController());
+    final controller = Get.put(NavigationController());
     final isDark = THelperFunctions.isDarkMode(context);
     return Scaffold(
       bottomNavigationBar: Obx(
-        () => NavigationBar(
-          height: 80,
-          elevation: 0.0,
-          backgroundColor: isDark ? TColors.dark : TColors.white,
-          indicatorColor: isDark ? TColors.primary.withOpacity(0.1) : TColors.primary.withOpacity(0.1),
-          selectedIndex: controller._selectedIndex.value,
-          onDestinationSelected: (int index){
+        () => BottomNavigationBar(
+          currentIndex: controller._selectedIndex.value,
+          onTap: (int index) {
             controller._selectedIndex.value = index;
           },
-          
-          destinations: [
-          NavigationDestination(icon: Icon(Iconsax.home), label: "Home"),
-          NavigationDestination(icon: Icon(Iconsax.shop), label: "Store"),
-          NavigationDestination(icon: Icon(Iconsax.shopping_cart), label: "Cart"),
-          NavigationDestination(icon: Icon(Iconsax.heart), label: "Wishlist"),
-          NavigationDestination(icon: Icon(Iconsax.user), label: "Profile"),
-        ]),
+          backgroundColor: isDark ? TColors.dark : TColors.white,
+          selectedItemColor: isDark ? TColors.secondary : TColors.primary, // Adjust as necessary
+          unselectedItemColor: isDark ? Colors.white70 : Colors.black54,
+          items: [
+            _buildBottomNavigationBarItem(Iconsax.home, "Home"),
+            _buildBottomNavigationBarItem(Iconsax.shop, "Store"),
+            _buildBottomNavigationBarItem(Iconsax.shopping_cart, "Cart"),
+            _buildBottomNavigationBarItem(Iconsax.heart, "Favourite"),
+            _buildBottomNavigationBarItem(Iconsax.user, "Profile"),
+          ],
+        ),
       ),
       body: Obx(() => controller.screenList[controller._selectedIndex.value]),
     );
   }
-}
 
+  BottomNavigationBarItem _buildBottomNavigationBarItem(IconData icon, String label) {
+    return BottomNavigationBarItem(
+      icon: _buildAnimatedIcon(icon),
+      label: _buildEllipsizedLabel(label),
+    );
+  }
+
+  Widget _buildAnimatedIcon(IconData icon) {
+    return AnimatedSwitcher(
+      duration: const Duration(milliseconds: 300), // Adjust the duration as necessary
+      transitionBuilder: (Widget child, Animation<double> animation) {
+        const offset = 0.1;
+        final tween = Tween<Offset>(begin: Offset(0, offset), end: Offset.zero);
+        final offsetAnimation = tween.animate(animation);
+        return SlideTransition(
+          position: offsetAnimation,
+          child: child,
+        );
+      },
+      child: Icon(
+        icon,
+        key: ValueKey<IconData>(icon), // Key to differentiate icons
+      ),
+    );
+  }
+
+  String _buildEllipsizedLabel(String label) {
+    // Return the label string for the BottomNavigationBarItem
+    return label.length > 8 ? label.substring(0, 8) + '...' : label;
+  }
+}
 
 class NavigationController extends GetxController {
   final Rx<int> _selectedIndex = 0.obs;
@@ -54,5 +81,4 @@ class NavigationController extends GetxController {
     FavouriteScreen(),
     SettingsScreen(),
   ];
-
 }
